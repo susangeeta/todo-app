689310bc91741b8d73c9a5b361703460b830fad6
Sangeeta <sangeetaswainlucky@gmail.com>
2025-10-21 19:31:34 +0530

    Header completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
     <section className="px-18 py-12 ">
-      <div className="bg-[#f8f9ff] p-9 border uppercase flex justify-center items-center text-5xl font-normal">
-        My ToDoList
+      <div className="bg-[#f8f9ff] text-[#5f6177] p-9  uppercase flex justify-center items-center text-5xl font-bold">
+        ToDoList
       </div>
     </section>
   );
