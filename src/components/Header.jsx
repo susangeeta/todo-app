a7c4796c37b472346c7d3271bef9eb1db84573e7
Sangeeta <sangeetaswainlucky@gmail.com>
2025-11-28 17:39:55 +0530

    some changes header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
-    <div className=" text-black uppercase flex justify-center items-center text-5xl font-bold">
-      My ToDos
+    <div className=" text-blue-600  uppercase flex justify-center items-center text-sm md:text-xl font-bold">
+      My ToDos List
     </div>
   );
 };
